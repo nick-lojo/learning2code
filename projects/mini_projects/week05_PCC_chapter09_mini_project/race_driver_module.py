b31ad798bb96505c151ab321e49e117cd6b42f8e
Nick Lojo <nicklojo24@gmail.com>
2026-03-18 13:30:10 +0000

    Saving driver modules

---
+++ projects/mini_projects/week05_PCC_chapter09_mini_project/race_driver_module.py
@@ -0,0 +1,25 @@
+from base_driver_module import BaseDriver as BD
+
+class RaceDriver(BD):
+    """A special class of base drivers."""
+    def __init__(self, driver_id, team, name, nationality, car_number, season_points=0):
+        """Defines attributes of the parent class and special attributes of the sub-class."""
+        super().__init__(driver_id, team, name, nationality, season_points)
+        self.car_number = car_number
+     
+    def display_diver(self):
+         """A special version that includes car number."""
+         super().display_diver()
+         print(f"Car Number: #{self.car_number}")
+
+class ReserveDriver(BD):
+    """A special class of base drivers."""
+    def __init__(self, driver_id, team, name, nationality, status, season_points=0):
+        """Defines common attributes and defines new ones."""
+        super().__init__(driver_id, team, name, nationality, season_points)
+        self.status = status
+    
+    def display_diver(self):
+        """A special version of display driver to show status."""
+        super().display_diver()
+        print(f"Status: {self.status.title()}")
