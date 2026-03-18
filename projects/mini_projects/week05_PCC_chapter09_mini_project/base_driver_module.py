2537a99cab328ad83f06f45f59a612702619b83a
Nick Lojo <nicklojo24@gmail.com>
2026-03-18 13:14:42 +0000

    Saving base driver module

---
+++ projects/mini_projects/week05_PCC_chapter09_mini_project/base_driver_module.py
@@ -0,0 +1,34 @@
+class BaseDriver:
+    """Models a base driver."""
+    def __init__(self, driver_id, team, name, nationality, season_points=0):
+        """Attributes shared by all drivers."""
+        self.driver_id = driver_id
+        self.team = team
+        self.name = name
+        self.nationality = nationality
+        self.season_points = season_points
+    
+    def display_diver(self):
+        """A method to display details about a driver."""
+        message = f"\nDriver ID: {self.driver_id}"
+        message += f"\n\tTeam: {self.team.title()}"
+        message += f"\n\tName: {self.name.title()}"
+        message += f"\n\tNationality: {self.nationality.title()}"
+        message += f"\n\tSeason Points: {self.season_points}"
+    
+    def add_points(self, place):
+        """A method to add points to a driver after winning a race."""
+        if place == '1st':
+            self.season_points += 25
+        elif place == '2nd':
+            self.season_points += 18
+        elif place == '3rd':
+            self.season_points += 15
+        elif place == '4th':
+            self.season_points += 12
+        elif place == '5th':
+            self.season_points += 10
+        elif place == '6th':
+            self.season_points += 8
+        
+        print(f"You now have {self.season_points} points on the season.")
