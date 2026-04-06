926ad7e84c4825bc909c43d614f33a3b809684f9
Nick Lojo <nicklojo24@gmail.com>
2026-04-06 14:30:55 +0000

    Save progress

--- lessons/week06_PCC_chapter11/name_function.py
+++ lessons/week06_PCC_chapter11/name_function.py
@@ -1,4 +1,4 @@
-def get_formatted_name(first, last):
+def get_formatted_name(first, last, middle=''):
     """Combine first and last name with a space between them."""
-    full_name = f"{first} {last}"
+    full_name = f"{first} {middle} {last}"
     return full_name.title()
