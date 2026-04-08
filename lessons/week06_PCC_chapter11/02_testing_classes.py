5a55aeaf60f8b2754b42ef4f5fa8d21870f1b45b
Nick Lojo <nicklojo24@gmail.com>
2026-04-08 17:45:35 +0000

    Save Progress

---
+++ lessons/week06_PCC_chapter11/02_testing_classes.py
@@ -0,0 +1,17 @@
+# A Variety of Assertions (PCC, pg. 217-218)
+
+# assert can check more than just equality
+# assert a == b: two values are equal
+# assert a != b: two values are not equal
+# assert a: a evaluates to True
+# assert not a: evaluates to False
+# assert element in list: element is in a list
+# assert element not in list: element is not in a list
+
+# Testing a Class (PCC, pg. 218-220)
+
+# Testing a class works similarly to testing a function
+# You test the behavior of the class's METHODS
+# To test a class, you need to create an instance of it first
+#   then call methods on the instance and assert the results
+
